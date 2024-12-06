4e2df36fa78aac165d1d17e303c9d60963d5bbc1
Dorde Dordevic <dodordev@c0c5c2.42berlin.de>
2024-12-06 13:46:01 +0100

    split and norminette of execute_commands in exec.c

--- srcs/executor/exec.c
+++ srcs/executor/exec.c
@@ -6,25 +6,45 @@
 /*   By: dodordev <dodordev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 18:04:13 by dodordev          #+#    #+#             */
-/*   Updated: 2024/12/04 17:21:52 by dodordev         ###   ########.fr       */
+/*   Updated: 2024/12/06 13:42:46 by dodordev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//TO DO: SPLIT
-int	execute_commands(t_shell *shell)
+static int	execute_pipeline(t_command *current, t_shell *shell,
+		struct sigaction *sa_old_int, struct sigaction *sa_old_quit)
 {
-	t_command			*current;
-	int					prev_pipe[2];
-	struct sigaction	sa_old_int;
-	struct sigaction	sa_old_quit;
-	pid_t				last_pid;
+	int		prev_pipe[2];
+	pid_t	last_pid;
 
-	current = shell->cmnd_lst;
 	prev_pipe[0] = -1;
 	prev_pipe[1] = -1;
 	last_pid = 0;
+	while (current)
+	{
+		if (!setup_pipeline_steps(current, prev_pipe, &last_pid, shell))
+		{
+			g_exit_status = 1;
+			sigaction(SIGINT, sa_old_int, NULL);
+			sigaction(SIGQUIT, sa_old_quit, NULL);
+			return (g_exit_status);
+		}
+		current = current->next;
+	}
+	sigaction(SIGINT, sa_old_int, NULL);
+	sigaction(SIGQUIT, sa_old_quit, NULL);
+	wait_for_children(last_pid);
+	return (g_exit_status);
+}
+
+int	execute_commands(t_shell *shell)
+{
+	t_command			*current;
+	struct sigaction	sa_old_int;
+	struct sigaction	sa_old_quit;
+
+	current = shell->cmnd_lst;
 	if (current && !current->is_valid)
 	{
 		print_command_not_found(current);
@@ -39,19 +59,5 @@ int	execute_commands(t_shell *shell)
 		sigaction(SIGQUIT, &sa_old_quit, NULL);
 		return (execute_single_builtin(current, shell));
 	}
-	while (current)
-	{
-		if (!setup_pipeline_steps(current, prev_pipe, &last_pid, shell))
-		{
-			g_exit_status = 1;
-			sigaction(SIGINT, &sa_old_int, NULL);
-			sigaction(SIGQUIT, &sa_old_quit, NULL);
-			return (g_exit_status);
-		}
-		current = current->next;
-	}
-	sigaction(SIGINT, &sa_old_int, NULL);
-	sigaction(SIGQUIT, &sa_old_quit, NULL);
-	wait_for_children(last_pid);
-	return (g_exit_status);
+	return (execute_pipeline(current, shell, &sa_old_int, &sa_old_quit));
 }
