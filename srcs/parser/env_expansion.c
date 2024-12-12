adf355365d83205eabe462de6dbd9b083caedd19
jadyar <jadyar@student.42berlin.de>
2024-12-12 16:49:45 +0100

    env_expansion TBD

--- srcs/parser/env_expansion.c
+++ srcs/parser/env_expansion.c
@@ -6,7 +6,7 @@
 /*   By: jadyar <jadyar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 17:46:12 by dodordev          #+#    #+#             */
-/*   Updated: 2024/12/12 16:12:58 by jadyar           ###   ########.fr       */
+/*   Updated: 2024/12/12 16:41:51 by jadyar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,39 +86,45 @@ char	*process_char(char *str, int *i, char *result, t_shell *shell)
 	return (result);
 }
 
-char *expand_env_vars(const char *str, t_shell *shell) {
-    char *result = ft_strdup("");
-    char *temp;
-    int i = 0;
-char *expand_env_vars(const char *str, t_shell *shell) {
-    char *result = ft_strdup("");
-    char *temp;
-    int i = 0;
+char	*expand_env_vars(char *str, t_shell *shell)
+{
+	char	*result;
+	char	*temp;
+	int		i;
 
-    if (!str || !shell)
-        return (NULL);
-    while (str[i]) {
-        if (str[i] == '\\' && (str[i + 1] == '$' || str[i + 1] == '\\')) {
-            temp = ft_strdup("$");
-            i += 2;
-        } else if (str[i] == '$' && str[i + 1] == '?') {
-            temp = ft_itoa(shell->exit_status);
-            i += 2;
-        } else if (str[i] == '$' && (ft_isalnum(str[i + 1]) || str[i + 1] == '_')) {
-            char *var_name = extract_env_var_name(&str[i + 1]);
-            if (var_name) {
-                temp = ft_strdup(get_env_value(var_name, shell) ?: "");
-                free(var_name);
-                i += ft_strlen(var_name) + 1;
-            } else {
-                temp = ft_strdup("");
-                i++;
-            }
-        } else {
-            temp = ft_substr(str, i++, 1);
-        }
-        result = ft_strjoin_free(result, temp);
-        free(temp);
-    }
-    return (result);
+	if (!str || !shell)
+		return (NULL);
+	result = ft_strdup("");
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\\' && (str[i + 1] == '$' || str[i + 1] == '\\'))
+		{
+			temp = ft_substr(str, i + 1, 1);
+			i += 2;
+		}
+		else if (str[i] == '$' && (ft_isalnum(str[i + 1]) || str[i + 1] == '_'))
+		{
+			temp = get_var_content(str, &i, shell);
+			if (!temp)
+				temp = ft_strdup("");
+		}
+		else if (str[i] == '$' && str[i + 1] == '?')
+		{
+			temp = ft_itoa(shell->exit_status);
+			i += 2;
+		}
+		else if (str[i] == '$' && (!str[i + 1] || ft_is_whitespace(str[i + 1])))
+		{
+			temp = ft_strdup("$");
+			i++;
+		}
+		else
+			temp = ft_substr(str, i++, 1);
+		result = ft_strjoin_free(result, temp);
+		free(temp);
+	}
+	return (result);
 }
